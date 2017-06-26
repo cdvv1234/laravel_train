e9ccffd4799fca74ff227b92a47753de2d279037
ming <cdvv1234@gmail.com>
2017-06-26 13:19:14 +0800

    first

---
+++ resources/views/layouts/partials/_header.blade.php
@@ -0,0 +1,2 @@
+<div class="brand">前瞻計畫-數位建設</div>
+<div class="address-bar">前瞻基礎建設計畫 | 五大建設 | 數位建設</div>
---
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -0,0 +1,52 @@
+<!-- Navigation -->
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+            <a class="navbar-brand" href="index.html">Business Casual</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href='/'>Home</a>
+                </li>
+                <li>
+                    <a href='/about'>最新消息</a>
+                </li>
+                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">數位建設</a>
+                  <ul class="dropdown-menu">
+                   <li><a href='/about'>計畫內容</a></li>
+                   <li><a href='/blog'>計畫目標</a></li>
+                   <li><a href='/contact'>推動做法</a></li>
+                   <li><a href='/contact'>工作指標</a></li>
+                   </ul>
+               </li>
+                <li>
+                    <a href='/show'>成果展示</a>
+                </li>
+                <li>
+                    <a href='/blog'>檔案上傳</a>
+                </li>
+                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">相關連結</a>
+                  <ul class="dropdown-menu">
+                   <li><a href='/about'>檔案下載</a></li>
+                   <li><a href='/blog'>相關單位</a></li>
+                  </ul>
+               </li>
+                <li>
+                    <a href='/'>Contact us</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
